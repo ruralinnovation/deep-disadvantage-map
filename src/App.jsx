2b61bc596e039c4ee95ff5548d245cef152d8552
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2023-12-28 12:06:20 -0500

    reworked controls, showing only rural

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import countyGeoJSON from './data/dd_pp_map.json';
 
 import CountyChoropleth from './components/CountyChoropleth';
 
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
+import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import Checkbox from '@mui/material/Checkbox';
+
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { filter } from 'd3';
@@ -60,67 +60,93 @@ const MAPBOX_TOKEN = 'pk.eyJ1IjoicnVyYWxpbm5vIiwiYSI6ImNqeHl0cW0xODBlMm0zY2x0dXl
 
 const App = () => {
 
-  const [data, setData] = useState(countyGeoJSON);
-  const [filter, setFilter] = useState("all");
+  const [data, setData] = useState({type: "FeatureCollection", features: countyGeoJSON.features.filter(d => d.properties.rural === 1)});
+  // const [filter, setFilter] = useState("all");
 
-  function handleChange(event) {
+  const [filter_state, setFilterState] = useState({
+    thriving: false,
+    comparator: false,
+  });
 
-    setFilter(event.target.value);
+  const handleChange = (event) => {
 
-    console.log("value is", event.target.value);
+    let updated_filter_state = ({
+      ...filter_state,
+      [event.target.name]: event.target.checked,
+    });
 
-    if (event.target.value === "all") {
-      setData(countyGeoJSON);
-    }
-
-    if (event.target.value === "rural") {
-      let rural_features = countyGeoJSON.features.filter(d => d.properties.rural === 1);
-      let rural_data = {type: "FeatureCollection", features: rural_features};
-      setData(rural_data);
-    }
-
-    if (event.target.value === "thriving") {
-      let thriving_features = countyGeoJSON.features.filter(d => thriving_communities.includes(d.properties.fips));
-      console.log("thriving ", thriving_features);
-      let thriving_data = {type: "FeatureCollection", features: thriving_features};
-      setData(thriving_data);
-    }
-
-    if (event.target.value === "comparator") {
-      let comparator_features = countyGeoJSON.features.filter(d => comparator_communities.includes(d.properties.fips));
-      console.log("comparator ", comparator_features);
-      let comparator_data = {type: "FeatureCollection", features: comparator_features};
-      setData(comparator_data);
-    }
+    setFilterState(updated_filter_state);
 
 
-  }
+    let updated_features = countyGeoJSON.features.filter(function(d) {
+
+      if (d.properties.rural === 0) {
+        return false;
+      }
+
+      if (updated_filter_state["thriving"] === false && updated_filter_state["comparator"] === false) {
+        return true;
+      }
+      else {
+        if (thriving_communities.includes(d.properties.fips) && updated_filter_state["thriving"] === true) {
+          return true;
+        }
+
+        if (comparator_communities.includes(d.properties.fips) && updated_filter_state["comparator"] === true) {
+          return true;
+        }
+      }
+
+      return false;
+
+    }) 
+
+    let updated_data = {type: "FeatureCollection", features: updated_features};
+
+    console.log(updated_features.length);
+
+    setData(updated_data);
+
+  };
 
   return (
     <>
       <ThemeProvider theme={theme}>
         <div className="App">
-          <div className="controls">
-            <h1>Deep disadvantage by county</h1>
-            <FormControl>
-              <FormLabel id="demo-row-radio-buttons-group-label"></FormLabel>
-              <RadioGroup
-                row
-                aria-labelledby="demo-row-radio-buttons-group-label"
-                defaultValue="all"
-                name="row-radio-buttons-group"
-                value={filter}
-                onChange={handleChange}
-              >
-                <FormControlLabel value="all" control={<Radio />} label="All" />
-                <FormControlLabel value="rural" control={<Radio />} label="Rural" />
-                <FormControlLabel value="thriving" control={<Radio />} label="Thriving" />
-                <FormControlLabel value="comparator" control={<Radio />} label="Comparator" />
-              </RadioGroup>
-            </FormControl>
+          <div className="map-header-footer">
+            <h1>Index of deep disadvantage</h1> 
+            <h2>For rural counties</h2>
+            <div className="controls">
+              <FormLabel className="controls-label" component="legend">Filter to:</FormLabel>
+              <FormGroup row>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={filter_state.thriving}
+                      onChange={handleChange}
+                      name="thriving"
+                    />
+                  }
+                  label="Thriving communities"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={filter_state.comparator}
+                      onChange={handleChange}
+                      name="comparator"
+                    />
+                  }
+                  label="Comparator communities"
+                />
+              </FormGroup>
+            </div>
           </div>
           <div className="map-container">
             <CountyChoropleth mapboxToken={MAPBOX_TOKEN} geojsonData={data} />
+          </div>
+          <div className="map-header-footer">
+            <p>Source: 2022 ACS 5-year estimates</p>
           </div>
         </div>
       </ThemeProvider>
